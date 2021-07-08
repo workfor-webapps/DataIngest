6bfebff2ac26f5807be3540c638a52ce3ea1a4fa
Daniel Marrable <marrabld@gmail.com>
2021-07-08 17:16:19 +0800

    Basic layout

--- main.py
+++ main.py
@@ -23,28 +23,28 @@ def PullTable():
     
     SCOPES = ['https://www.googleapis.com/auth/drive']
 
-    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-    files = get_files(service)
-    file1 = files[0]
-    file_id = file1["id"]
-    get_temp_pdf(service, file_id)
-    #path = os.getcwd() + "/temp/"
-    temp_file = "temp.pdf"
-    paper_title = get_title(temp_file)
-    table_clean = extract_tables(temp_file)
-    df = table_clean[0]
-   
-    #-----------------Get json for table-------------
-    data = df.to_json(orient='table')
-    #------------------------------------------------
-    
-    html_file = df.to_html(index=False, justify="center" )
-    text_file = open("./templates/indexT.html", "w")
-    header = "{% extends 'base.html' %}\n{% block head %}\n<h2>Table 1</h2>\n{% endblock %}\n<br>\n{% block body %}\n<img src='../static/capture.png' >\n"
-    text_file.write(header)
-    text_file.write(html_file)
-    text_file.write("{% endblock %}")
-    text_file.close() 
+#     service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+#     files = get_files(service)
+#     file1 = files[0]
+#     file_id = file1["id"]
+#     get_temp_pdf(service, file_id)
+#     #path = os.getcwd() + "/temp/"
+#     temp_file = "temp.pdf"
+#     paper_title = get_title(temp_file)
+#     table_clean = extract_tables(temp_file)
+#     df = table_clean[0]
+#
+#     #-----------------Get json for table-------------
+#     data = df.to_json(orient='table')
+#     #------------------------------------------------
+#
+#     html_file = df.to_html(index=False, justify="center" )
+#     text_file = open("./templates/indexT.html", "w")
+#     header = "{% extends 'base.html' %}\n{% block head %}\n<h2>Table 1</h2>\n{% endblock %}\n<br>\n{% block body %}\n<img src='../static/capture.png' >\n"
+#     text_file.write(header)
+#     text_file.write(html_file)
+#     text_file.write("{% endblock %}")
+#     text_file.close()
     #paths = os.getcwd() + "/src/temp/Capture.PNG"
     return render_template('indexT.html')
     
